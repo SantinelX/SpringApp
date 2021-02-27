5b6fdafd8ca93e0cf4183b86f03f4b5e2d764bf5
Calin Balazs <balazs_calin@yahoo.com>
2021-02-27 11:47:20 +0200

    timelife implementation

--- src/main/java/com/SDA/company/controller/CompanyControllerThymeleaf.java
+++ src/main/java/com/SDA/company/controller/CompanyControllerThymeleaf.java
@@ -1,8 +1,15 @@
 package com.SDA.company.controller;
 
+import com.SDA.company.models.Company;
 import com.SDA.company.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+import java.util.List;
 
 @Controller
 public class CompanyControllerThymeleaf {
@@ -12,5 +19,12 @@ public class CompanyControllerThymeleaf {
     @Autowired
     public CompanyControllerThymeleaf(CompanyService companyService) {
         this.companyService = companyService;
+    }
+
+    @RequestMapping(value = "/getAll", method = RequestMethod.GET)
+    public String getAllCompanies(Model model){
+        List<Company> companyList = companyService.getAllCompanies();
+        model.addAttribute("list",companyList);
+        return "companyTable";
     }
 }
